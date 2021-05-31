c38c35677c9d637dc3845fd0baa6b735ac1f06ca
Diego Lopez <dilogi7@gmail.com>
2021-05-31 10:14:12 -0500

    worflow

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import type {HttpFunction} from '@google-cloud/functions-framework/build/src/functions';
 
 export const sendEmail: HttpFunction = (req, res) => {
-  res.send(JSON.stringify({headers: req.headers, body: req.body}));
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify({headers: req.headers, body: req.body}, null, 4));
 };
